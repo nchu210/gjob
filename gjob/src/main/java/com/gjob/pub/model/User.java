191bd74a844776d91b28868b635e22cf2374f59e
Orient <1156373411@qq.com>
2019-07-10 22:04:03 +0800

    一个简单的接口

---
+++ gjob/src/main/java/com/gjob/pub/model/User.java
@@ -0,0 +1,54 @@
+package com.gjob.pub.model;
+
+/**
+ * 用户实体类
+ */
+public class User {
+
+    private String userName;
+    private String age;
+    private String sex;
+    private String address;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", age='" + age + '\'' +
+                ", sex='" + sex + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
